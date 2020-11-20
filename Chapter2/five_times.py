54fde7b20b23eeca9157bd5e796174de435c1eff
Will Henry <willhenry717@gmail.com>
2020-11-20 10:12:46 -0500

    Five times with while

--- Chapter2/five_times.py
+++ Chapter2/five_times.py
@@ -1,4 +1,10 @@
 print('My name is')
 
-for i in range(5):
+#for i in range(5):
+#    print('Jimmy Five Times (' + str(i) + ')')
+
+# Same thing with a while\
+i = 0
+while i < 5:
     print('Jimmy Five Times (' + str(i) + ')')
+    i = i + 1

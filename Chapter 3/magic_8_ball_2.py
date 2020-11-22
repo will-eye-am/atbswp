e03d96e60440c59b4cf9c4578733237bc43c76ab
Will Henry <willhenry717@gmail.com>
2020-11-22 11:15:02 -0500

    Magic 8 Ball 2

---
+++ Chapter 3/magic_8_ball_2.py
@@ -0,0 +1,12 @@
+import random
+
+fortunes = ['It is certain',
+    'It is decidely so',
+    'Reply hazy, try again',
+    'Ask again later',
+    'Concentrate and ask again',
+    'My reply is no',
+    'Outlook not so good',
+    'Very doubtful']
+
+print(random.choice(fortunes))
